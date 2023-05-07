afc0d48ca32a6bbfc1358a7c0051a3385edda111
Redwan <mdrhaq.qa@gmail.com>
2023-05-07 01:15:58 -0400

    Create c.java

---
+++ c.java
@@ -0,0 +1,10 @@
+public class c {
+        public static void main(String[]args){
+            String a= "hello";
+            String b="World";
+            String c=a.concat(b);
+            System.out.println(c);
+        }
+    }
+
+}
