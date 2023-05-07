85cd57442965db475a99db3316b813d4f5f0545b
Redwan <mdrhaq.qa@gmail.com>
2023-05-07 01:14:46 -0400

    updating r

---
+++ r.java
@@ -0,0 +1,8 @@
+public class r {
+    public static void main(String[]args){
+        String a= "hello";
+        String b="World";
+        String c=a.concat(b);
+        System.out.println(c);
+    }
+}
